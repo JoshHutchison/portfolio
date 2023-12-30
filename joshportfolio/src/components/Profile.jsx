e7126516ec29b3b72e916cfa254401821f999605
Josh Hutchison <joshhutchison@hotmail.com>
2023-12-29 19:32:40 -0500

    updated the icons and the hover effects

--- joshportfolio/src/components/Profile.jsx
+++ joshportfolio/src/components/Profile.jsx
@@ -1,33 +1,110 @@
-import profilePic from "../assets/profilepic.jpg"
+import profilePic from "../assets/profilepic.jpg";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faFileLines } from "@fortawesome/free-solid-svg-icons"
 
 const Profile = () => {
   return (
-    <section id='home' className="relative flex items-center w-full bg-black">
-                <div className="relative items-center w-full px-5 py-24 mx-auto md:px-12 sm:px-16 max-w-7xl">
-                  <div className="relative flex-col items-start m-auto align-middle">
-                    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 sm:gap-24">
-                      <div className="relative items-center gap-12 m-auto sm:inline-flex">
-                        <div className="max-w-xl text-center sm:text-left">
-                          <div>
-                           
-                            <p className="text-xl font-medium tracking-tight text-white sm:text-6xl">
-                            Crafting exceptional solutions through code mastery
-                            </p>
-                            <p className="max-w-xl mt-4 text-base tracking-tight text-gray-600">
-                            Explore Josh Hutchison's innovative software solutions, enhancing your business efficiency with top-tier developer expertise
-                            </p>
-                          </div>
-                          
-                        </div>
-                      </div>
-                      <div className="order-first block w-full mt-12 aspect-square sm:mt-0 sm:order-first">
-                        <img className="object-cover object-center w-full mx-auto bg-gray-300 sm:ml-auto" alt="hero" src={profilePic} />
-                      </div>
-                    </div>
-                  </div>
+    <section id="home" className="relative flex items-center w-full bg-black">
+      <div className="relative items-center w-full px-5 py-24 mx-auto md:px-12 md:px-16 max-w-7xl">
+        <div className="relative flex-col items-start m-auto align-middle">
+          <div className="grid grid-cols-1 gap-6 md:grid-cols-2 md:gap-24">
+            <div className="relative items-center gap-12 m-auto md:inline-flex">
+              <div className="max-w-xl text-center md:text-left">
+                <div>
+                  <p className="text-xl font-medium tracking-tight text-white md:text-2xl">
+                    Josh Hutchison | Full Stack Developer
+                  </p>
+                  <p className="text-xl font-medium tracking-tight text-white md:text-3xl lg:text-6xl">
+                    Crafting exceptional solutions through code mastery
+                  </p>
+                  <p className="max-w-xl mt-4 text-base tracking-tight text-gray-600">
+                    Hey there, I'm Josh 👋, a seasoned software developer with a
+                    knack for problem-solving and a background in electronic
+                    engineering. My journey into software began in roles
+                    spanning software automation, systems integration, and
+                    technical support, allowing me to cultivate a robust skill
+                    set in Java, Python, and Web Development over the past 5
+                    years.
+                  </p>
+                  <p className="max-w-xl mt-4 text-base tracking-tight text-gray-600">
+                    Beyond coding, my interests lie in video games, biking, and
+                    snowboarding. These passions provide a refreshing break and
+                    often infuse my problem-solving approach with creativity and
+                    strategic thinking. Whether conquering virtual worlds or
+                    navigating challenging terrains, these hobbies nurture my
+                    resilience and adaptability—qualities that seamlessly
+                    translate into my work in software development. I'm
+                    enthusiastic about exploring opportunities where I can
+                    leverage my technical expertise, problem-solving prowess,
+                    and passion for innovation to contribute meaningfully.
+                    Looking forward to connecting and embarking on exciting
+                    projects together!
+                  </p>
                 </div>
-              </section>
-  )
-}
+                <div className="flex justify-center mt-8 space-x-6">
+                  <span className="inline-flex justify-center w-full gap-3 m-auto md:justify-start md:w-auto">
+                    <div className="px-5 py-2">
+                      <a
+                        href="https://www.linkedin.com/in/josh-g-hutchison/"
+                        target="_blank"
+                        className="text-sm text-gray-500 hover:text-blue-600"
+                      >
+                        <FontAwesomeIcon
+                          icon={faLinkedin}
+                          size="xl"
+                          className="px-2"
+                        />
+                        LinkedIn
+                      </a>
+                    </div>
+                    <div className="px-5 py-2">
+                      <a
+                        href="https://github.com/JoshHutchison"
+                        target="_blank"
+                        className="text-sm text-gray-500 hover:text-blue-600"
+                      >
+                        <FontAwesomeIcon
+                          icon={faGithub}
+                          size="xl"
+                          className="px-2"
+                        />
+                        Github
+                      </a>
+                    </div>
+                    <div className="px-5 py-2">
+                    <a
+                  className="text-sm text-gray-500 hover:text-blue-600"
+                  href="/src/assets/JoshHutchison.pdf"
+                  tager="_blank"
+                  download
+                >
+                  <FontAwesomeIcon
+                          icon={faFileLines}
+                          size="xl"
+                          className="px-2"
+                        />
+                  Resume
+                </a>
+                </div>
+                  </span>
+                </div>
+                
+              </div>
+            </div>
+            <div className="order-first block w-full mt-12 aspect-square sm:mt-0 sm:order-first">
+              <img
+                className="object-cover object-center w-full mx-auto bg-gray-300 sm:ml-auto"
+                alt="hero"
+                src={profilePic}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Profile
+export default Profile;
--- joshportfolio/src/components/Projects.jsx
+++ joshportfolio/src/components/Projects.jsx
@@ -1,39 +1,73 @@
-
-
 import React from 'react';
-
-import monsterAdoptionGif from '../assets/monsterAdoption.gif'
-import mernPizzaGif from '../assets/mernPizza.gif'
-import gamingDealsGif from '../assets/gamingDeals.gif'
-import jediGamePng from '../assets/JediGame.png'
-
+import monsterAdoptionGif from '../assets/monsterAdoption.gif';
+import mernPizzaGif from '../assets/mernPizza.gif';
+import gamingDealsGif from '../assets/gamingDeals.gif';
+import jediGamePng from '../assets/JediGame.png';
 
 const Projects = () => {
   const projectData = [
     {
-      name: 'Monster Adoption',
+      name: 'Monster Pet Adoption',
       githubLink: 'https://github.com/JoshHutchison/MonsterAdoption',
-      liveLink: 'https://github.com/JoshHutchison/MonsterAdoption', 
+      liveLink: 'https://github.com/JoshHutchison/MonsterAdoption',
       imageSrc: monsterAdoptionGif,
+      description: 'A Monster Pet Adoption Website, presenting various monsters to be adopted.',
+      technologies: [
+        { label: 'React', icon: 'react', color: 'blue' },
+        { label: 'Node.js', icon: 'nodedotjs', color: 'blue' },
+        { label: 'MongoDB', icon: 'mongodb', color: 'blue' },
+      ],
     },
     {
       name: 'Gaming Deals',
       githubLink: 'https://github.com/JoshHutchison/GamingDeals',
-      liveLink: 'https://github.com/JoshHutchison/GamingDeals', 
-      imageSrc: gamingDealsGif, 
+      liveLink: 'https://github.com/JoshHutchison/GamingDeals',
+      imageSrc: gamingDealsGif,
+      description: 'A Gaming Deals Website, offering a comprehensive list of gaming discounts and deals from various sources.',
+      technologies: [
+        { label: 'HTML', icon: 'html5', color: 'blue' },
+        { label: 'CSS', icon: 'css3', color: 'blue' },
+        { label: 'JavaScript', icon: 'javascript', color: 'blue' },
+        { label: 'MongoDB', icon: 'mongodb', color: 'blue' },
+        { label: 'Express', icon: 'express', color: 'black' },
+      ],
     },
     {
-      name: 'MERN Pizza Haus',
+      name: 'Pizza Shop',
       githubLink: 'https://github.com/JoshHutchison/Pizza-HAUS',
-      liveLink: 'https://github.com/JoshHutchison/Pizza-HAUS', 
-      imageSrc: mernPizzaGif, 
+      liveLink: 'https://github.com/JoshHutchison/Pizza-HAUS',
+      imageSrc: mernPizzaGif,
+      description: 'A Pizza Shop Website using React.js for the front-end and integrated it with a MongoDB database.',
+      technologies: [
+        { label: 'React', icon: 'react', color: 'blue' },
+        { label: 'Node.js', icon: 'nodedotjs', color: 'blue' },
+        { label: 'MongoDB', icon: 'mongodb', color: 'blue' },
+      ],
     },
     {
       name: 'Jedi Game',
       githubLink: 'https://github.com/JoshHutchison/Jedi-survival',
       liveLink: 'https://jedi-survive.surge.sh/',
-      imageSrc: jediGamePng, 
-    }
+      imageSrc: jediGamePng,
+      description: 'A survival game where the player reflects blaster shots with their lightsaber.',
+      technologies: [
+        { label: 'HTML', icon: 'html5', color: 'blue' },
+        { label: 'JavaScript', icon: 'javascript', color: 'blue' },
+        { label: 'CSS', icon: 'css3', color: 'blue' },
+        { label: 'HTML5 Canvas API', icon: 'html5', color: 'blue' },
+      ],
+    },
+  ];
+
+  const techItems = [
+    { label: 'HTML', icon: 'html5', color: 'blue' },
+    { label: 'CSS', icon: 'css3', color: 'blue' },
+    { label: 'JavaScript', icon: 'javascript', color: 'blue' },
+    { label: 'React', icon: 'react', color: 'blue' },
+    { label: 'Node.js', icon: 'nodedotjs', color: 'blue' },
+    { label: 'MongoDB', icon: 'mongodb', color: 'blue' },
+    { label: 'Express', icon: 'express', color: 'black' },
+
   ];
 
   return (
@@ -43,26 +77,52 @@ const Projects = () => {
         {projectData.map((project, index) => (
           <div key={index} className="flex flex-col items-center">
             <h3 className="mb-2">{project.name}</h3>
-            
-            <div className="w-64 h-40">
+
+            <div className="w-64 h-40 relative">
               <img
                 src={project.imageSrc}
                 alt={project.name}
                 className="w-full h-full object-cover"
               />
+              <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center opacity-0 transition-opacity duration-300 bg-black bg-opacity-80 hover:opacity-100">
+                <p className="text-white text-center">{project.description}</p>
+                
+              </div>
             </div>
+
+            <div className="flex mt-2">
+                  {project.technologies.map((tech, idx) => {
+                    const foundTech = techItems.find((item) => item.label === tech.label);
+                    if (foundTech) {
+                      return (
+                        <div key={idx} className="mx-1">
+                          <img
+                            src={`https://img.shields.io/badge/${foundTech.label}-black?style=flat-square&logo=${foundTech.icon}&logoColor=white&labelColor=black`}
+                            alt={foundTech.label}
+                            className="w-16 h-8"
+                          />
+                        </div>
+                      );
+                    }
+                    return null;
+                  })}
+                </div>    
             <div className="flex justify-center gap-2 mb-2">
-              <a href={project.githubLink} target="_blank">Github</a>
+              <a href={project.githubLink} target="_blank" rel="noopener noreferrer">
+                Github
+              </a>
               <span>&nbsp;</span>
               <p>|</p>
               <span>&nbsp;</span>
-              <a href={project.liveLink} target="_blank">Live</a>
+              <a href={project.liveLink} target="_blank" rel="noopener noreferrer">
+                Live
+              </a>
             </div>
           </div>
         ))}
       </div>
     </div>
   );
-        }
+};
 
 export default Projects;
