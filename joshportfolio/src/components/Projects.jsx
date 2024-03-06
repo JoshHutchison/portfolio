37b80a8488c75b9a7e76d1fbff14171d0e8a456b
Josh Hutchison <joshhutchison@hotmail.com>
2024-03-06 16:48:16 -0500

    added skill icons to the projects component

--- joshportfolio/src/components/Projects.jsx
+++ joshportfolio/src/components/Projects.jsx
@@ -1,27 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import monsterAdoptionGif from '../assets/monsterAdoption.gif';
 import mernPizzaGif from '../assets/mernPizza.gif';
 import gamingDealsGif from '../assets/gamingDeals.gif';
 import jediGamePng from '../assets/JediGame.png';
+import { skills } from './Skills';
+import tippy from 'tippy.js';
+import 'tippy.js/dist/tippy.css';
 
 const Projects = () => {
+  useEffect(() => {
+    // Initialize the tooltips for each skill in the project technologies
+    skills.forEach((skill) => {
+      tippy(`.${skill.name.toLowerCase()}`, {
+        content: skill.name,
+        delay: [5, 5], // [showDelay, hideDelay] in milliseconds
+      });
+    });
+  }, []);
+
   const projectData = [
     {
       name: 'Monster Pet Adoption',
       githubLink: 'https://github.com/JoshHutchison/MonsterAdoption',
-      liveLink: 'https://github.com/JoshHutchison/MonsterAdoption',
+      liveLink: '',
       imageSrc: monsterAdoptionGif,
       description: 'A Monster Pet Adoption Website, presenting various monsters to be adopted.',
       technologies: [
         { label: 'React', icon: 'react', color: 'blue' },
-        { label: 'Node.js', icon: 'nodedotjs', color: 'blue' },
-        { label: 'MongoDB', icon: 'mongodb', color: 'blue' },
+        { label: 'Python', icon: 'python', color: 'blue' },
+        { label: 'Django', icon: 'django', color: 'blue' },
+        { label: 'Postgres', icon: 'postgresql', color: 'blue' },
       ],
     },
     {
       name: 'Gaming Deals',
       githubLink: 'https://github.com/JoshHutchison/GamingDeals',
-      liveLink: 'https://github.com/JoshHutchison/GamingDeals',
+      liveLink: '',
       imageSrc: gamingDealsGif,
       description: 'A Gaming Deals Website, offering a comprehensive list of gaming discounts and deals from various sources.',
       technologies: [
@@ -35,12 +49,12 @@ const Projects = () => {
     {
       name: 'Pizza Shop',
       githubLink: 'https://github.com/JoshHutchison/Pizza-HAUS',
-      liveLink: 'https://github.com/JoshHutchison/Pizza-HAUS',
+      liveLink: 'https://pizzahaus.netlify.app/',
       imageSrc: mernPizzaGif,
       description: 'A Pizza Shop Website using React.js for the front-end and integrated it with a MongoDB database.',
       technologies: [
         { label: 'React', icon: 'react', color: 'blue' },
-        { label: 'Node.js', icon: 'nodedotjs', color: 'blue' },
+        { label: 'Node', icon: 'nodejs', color: 'blue' },
         { label: 'MongoDB', icon: 'mongodb', color: 'blue' },
       ],
     },
@@ -59,17 +73,6 @@ const Projects = () => {
     },
   ];
 
-  const techItems = [
-    { label: 'HTML', icon: 'html5', color: 'blue' },
-    { label: 'CSS', icon: 'css3', color: 'blue' },
-    { label: 'JavaScript', icon: 'javascript', color: 'blue' },
-    { label: 'React', icon: 'react', color: 'blue' },
-    { label: 'Node.js', icon: 'nodedotjs', color: 'blue' },
-    { label: 'MongoDB', icon: 'mongodb', color: 'blue' },
-    { label: 'Express', icon: 'express', color: 'black' },
-
-  ];
-
   return (
     <div id='apps' className="text-white my-4 text-center">
       <h2 className="text-4xl mb-4">Projects</h2>
@@ -86,37 +89,43 @@ const Projects = () => {
               />
               <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center opacity-0 transition-opacity duration-300 bg-black bg-opacity-80 hover:opacity-100">
                 <p className="text-white text-center">{project.description}</p>
-                
               </div>
             </div>
 
             <div className="flex mt-2">
-                  {project.technologies.map((tech, idx) => {
-                    const foundTech = techItems.find((item) => item.label === tech.label);
-                    if (foundTech) {
-                      return (
-                        <div key={idx} className="mx-1">
-                          <img
-                            src={`https://img.shields.io/badge/${foundTech.label}-black?style=flat-square&logo=${foundTech.icon}&logoColor=white&labelColor=black`}
-                            alt={foundTech.label}
-                            className="w-16 h-8"
-                          />
-                        </div>
-                      );
-                    }
-                    return null;
-                  })}
-                </div>    
+              {project.technologies.map((tech, idx) => {
+                const foundSkill = skills.find((skill) => skill.name === tech.label);
+                if (foundSkill) {
+                  return (
+                    <div key={idx} className={`mx-1 ${foundSkill.name.toLowerCase()}`}>
+                      <img
+                        src={`https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/${foundSkill.icon}`}
+                        width="24"
+                        height="24"
+                        alt={foundSkill.name}
+                        title={foundSkill.name}
+                      />
+                    </div>
+                  );
+                }
+                return null;
+              })}
+            </div>
+
             <div className="flex justify-center gap-2 mb-2">
               <a href={project.githubLink} target="_blank" rel="noopener noreferrer">
                 Github
               </a>
-              <span>&nbsp;</span>
-              <p>|</p>
-              <span>&nbsp;</span>
-              <a href={project.liveLink} target="_blank" rel="noopener noreferrer">
-                Live
-              </a>
+              {project.liveLink && (
+                <>
+                  <span>&nbsp;</span>
+                  <p>|</p>
+                  <span>&nbsp;</span>
+                  <a href={project.liveLink} target="_blank" rel="noopener noreferrer">
+                    Live
+                  </a>
+                </>
+              )}
             </div>
           </div>
         ))}
--- joshportfolio/src/components/Skills.jsx
+++ joshportfolio/src/components/Skills.jsx
@@ -2,30 +2,37 @@ import { useEffect } from 'react';
 import tippy from 'tippy.js';
 import 'tippy.js/dist/tippy.css';
 
+const skills = [
+  { name: 'JavaScript', link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript', icon: 'javascript-colored.svg' },
+  { name: 'Python', link: 'https://www.python.org/', icon: 'python-colored.svg' },
+  { name: 'CSharp', link: 'https://docs.microsoft.com/en-us/dotnet/csharp/', icon: 'csharp-colored.svg' },
+  { name: 'Git', link: 'https://git-scm.com/', icon: 'git-colored.svg' },
+  { name: 'Java', link: 'https://www.oracle.com/java/', icon: 'java-colored.svg' },
+  { name: 'HTML5', link: 'https://developer.mozilla.org/en-US/docs/Glossary/HTML5', icon: 'html5-colored.svg' },
+  { name: 'React', link: 'https://reactjs.org/', icon: 'react-colored.svg' },
+  { name: 'CSS3', link: 'https://www.w3.org/TR/CSS/#css', icon: 'css3-colored.svg' },
+  { name: 'TailwindCSS', link: 'https://tailwindcss.com/', icon: 'tailwindcss-colored.svg' },
+  { name: 'Bootstrap', link: 'https://getbootstrap.com/', icon: 'bootstrap-colored.svg' },
+  { name: 'Vite', link: 'https://vitejs.dev/', icon: 'vite-colored.svg' },
+  { name: 'NodeJS', link: 'https://nodejs.org/en/', icon: 'nodejs-colored.svg' },
+  { name: 'Express', link: 'https://expressjs.com/', icon: 'express.svg' },
+  { name: 'Oracle', link: 'https://www.oracle.com/uk/index.html', icon: 'oracle-colored.svg' },
+  { name: 'MongoDB', link: 'https://www.mongodb.com/', icon: 'mongodb-colored.svg' },
+  { name: 'PostgreSQL', link: 'https://www.postgresql.org/', icon: 'postgresql-colored.svg' },
+  { name: 'Django', link: 'https://www.djangoproject.com/', icon: 'django-colored.svg' },
+  { name: 'AWS', link: 'https://aws.amazon.com', icon: 'aws-colored.svg' },
+  { name: 'Docker', link: 'https://www.docker.com/', icon: 'docker-colored.svg' },
+  { name: 'Linux', link: 'https://www.linux.org', icon: 'linux-colored.svg' },
+  { name: 'MacOS', link: 'https://apple.com', icon: 'macos.svg' },
+  { name: 'HTML', link: 'https://apple.com', icon: 'html5-colored.svg' },
+  { name: 'CSS', link: 'https://apple.com', icon: 'css3-colored.svg' },
+  { name: 'Postgres', link: 'https://apple.com', icon: 'postgresql-colored.svg' },
+  { name: 'Node', link: 'https://apple.com', icon: 'nodejs-colored.svg' },
+
+];
+
 const Skills = () => {
-  const skills = [
-    { name: 'JavaScript', link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript', icon: 'javascript-colored.svg' },
-    { name: 'Python', link: 'https://www.python.org/', icon: 'python-colored.svg' },
-    { name: 'CSharp', link: 'https://docs.microsoft.com/en-us/dotnet/csharp/', icon: 'csharp-colored.svg' },
-    { name: 'Git', link: 'https://git-scm.com/', icon: 'git-colored.svg' },
-    { name: 'Java', link: 'https://www.oracle.com/java/', icon: 'java-colored.svg' },
-    { name: 'HTML5', link: 'https://developer.mozilla.org/en-US/docs/Glossary/HTML5', icon: 'html5-colored.svg' },
-    { name: 'React', link: 'https://reactjs.org/', icon: 'react-colored.svg' },
-    { name: 'CSS3', link: 'https://www.w3.org/TR/CSS/#css', icon: 'css3-colored.svg' },
-    { name: 'TailwindCSS', link: 'https://tailwindcss.com/', icon: 'tailwindcss-colored.svg' },
-    { name: 'Bootstrap', link: 'https://getbootstrap.com/', icon: 'bootstrap-colored.svg' },
-    { name: 'Vite', link: 'https://vitejs.dev/', icon: 'vite-colored.svg' },
-    { name: 'NodeJS', link: 'https://nodejs.org/en/', icon: 'nodejs-colored.svg' },
-    { name: 'Express', link: 'https://expressjs.com/', icon: 'express.svg' },
-    { name: 'Oracle', link: 'https://www.oracle.com/uk/index.html', icon: 'oracle-colored.svg' },
-    { name: 'MongoDB', link: 'https://www.mongodb.com/', icon: 'mongodb-colored.svg' },
-    { name: 'PostgreSQL', link: 'https://www.postgresql.org/', icon: 'postgresql-colored.svg' },
-    { name: 'Django', link: 'https://www.djangoproject.com/', icon: 'django-colored.svg' },
-    { name: 'AWS', link: 'https://aws.amazon.com', icon: 'aws-colored.svg' },
-    { name: 'Docker', link: 'https://www.docker.com/', icon: 'docker-colored.svg' },
-    { name: 'Linux', link: 'https://www.linux.org', icon: 'linux-colored.svg' },
-    { name: 'MacOS', link: 'https://apple.com', icon: 'macos.svg' },
-  ];
+  
   useEffect(() => {
     // Initialize the tooltips for each skill
     
@@ -67,3 +74,4 @@ const Skills = () => {
 };
 
 export default Skills;
+export { skills };
